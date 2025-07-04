01110e3da2d0f31e05a838cd83abad8149d54ace
MGDArrow <PSMuz@mgdmail.ru>
2025-07-04 23:51:11 +0300

    init test

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="temp">
+    <p>{{ test }}</p>
+    <button type="button" @click="test++">+</button>
+    <button type="button" @click="test--">-</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const test = ref(0);
+</script>
