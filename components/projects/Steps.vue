60455496731dd6660d256ac7964548acd7bccc6c
MGD-Prog <PSMuz@mgdmail.ru>
2026-03-01 16:46:19 +0300

    Add sort in steps in projects

---
+++ components/projects/Steps.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="project-steps">
+    <h1>Шаги проекта</h1>
+    <div class="project-steps__buttons">
+      <button @click="isSorted = false" :class="{ active: !isSorted }">
+        Сначала новые
+      </button>
+      <button @click="isSorted = true" :class="{ active: isSorted }">
+        Сначала старые
+      </button>
+    </div>
+
+    <div class="project-steps__body">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineSlots<{
+    default: string;
+  }>();
+
+  const isSorted = ref(false);
+  const direction = computed(() => {
+    return isSorted.value ? 'column-reverse' : 'column';
+  });
+</script>
+
+<style scoped lang="scss">
+  .project-steps {
+    width: 100%;
+    margin-top: 20px;
+    &__buttons {
+      display: flex;
+      justify-content: center;
+      gap: min(20px, 2vw);
+      margin: 0 auto;
+      & button {
+        font-weight: 600;
+        font-size: 1em;
+        background: transparent;
+        cursor: pointer;
+        padding: 10px min(20px, 1vw);
+        border: 4px solid var(--color-black);
+        border-radius: var(--border-radius);
+        transition: 0.3s ease-in-out;
+        &.active {
+          background: var(--color-black);
+          color: var(--color-white);
+        }
+      }
+    }
+    &__body {
+      display: flex;
+      flex-direction: v-bind(direction);
+    }
+  }
+</style>
