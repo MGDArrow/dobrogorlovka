5db8b03a25e6ddc4db33f71e9fdc6025cb2a9cc1
MGDArrow <PSMuz@mgdmail.ru>
2026-06-20 23:29:21 +0300

    Удаление лишней функции

--- utils/utils.ts
+++ utils/utils.ts
@@ -18,29 +18,6 @@ export function scrollActivation(
   if (elementTop <= needScroll) activated.value = true;
 }
 
-export function scrollActivationLeft(
-  ref: HTMLElement | null,
-  parent: HTMLElement | null,
-  activated: Ref<boolean>,
-  PERCENT = 0.45,
-) {
-  if (activated.value === true) return;
-  if (ref === null) return;
-  if (parent === null) return;
-
-  const doc = document.documentElement;
-  if (doc.clientWidth > 768) return;
-
-  const needScroll = doc.clientHeight * PERCENT + doc.scrollTop;
-  const elementTop = parent?.offsetTop;
-
-  const needScrollLeft = ref.offsetLeft - ref.clientWidth * PERCENT;
-  const elementLeft = parent?.scrollLeft as number;
-
-  if (elementTop <= needScroll && needScrollLeft <= elementLeft)
-    activated.value = true;
-}
-
 export function getRandomColor() {
   const number = Math.random();
   return number > 0.75
